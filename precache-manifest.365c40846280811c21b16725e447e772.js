7b10afab1d3b4999502bc1014c518ebca41ea601
KIRTISHD <aniket.dhande@gmail.com>
2020-08-16 12:38:42 +0530

    Updates

---
+++ precache-manifest.365c40846280811c21b16725e447e772.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27df26fa833835984519cff51324f949",
+    "url": "/hangman-react/index.html"
+  },
+  {
+    "revision": "b9c6dd35f1a23a84b048",
+    "url": "/hangman-react/static/css/main.57800e22.chunk.css"
+  },
+  {
+    "revision": "99ca1499a5d0b54d3446",
+    "url": "/hangman-react/static/js/2.5cfe9452.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman-react/static/js/2.5cfe9452.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9c6dd35f1a23a84b048",
+    "url": "/hangman-react/static/js/main.350c941a.chunk.js"
+  },
+  {
+    "revision": "905b18a6683e481c2033",
+    "url": "/hangman-react/static/js/runtime-main.fcc5d5bd.js"
+  }
+]);
